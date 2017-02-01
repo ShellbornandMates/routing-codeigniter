05e22f9092945af099a265e7d0df6959811f96d0
rshellborn <rachel@shellborn.com>
2017-02-01 11:56:40 -0800

    closes #9 task, added regex routing rule and controller for displaying socrates quote

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['lock/(:any)/(:any)'] = 'welcome/shucks';
 $route['sleep'] = "first/zzz";
+$route['^[a-zA-Z]{4}/bingo'] = "bingo";
