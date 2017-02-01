f089031ed2ad71fb667979294657a4f350535820
shanejackson <sjacksondev@gmail.com>
2017-02-01 11:03:43 -0800

    closed tast #1

---
+++ application/controllers/First.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class First extends Application
+{
+    public function index() {
+        // this is the view we want shown
+		$record = $this->quotes->get('6');
+        echo $record['what'];
+    }
+}
